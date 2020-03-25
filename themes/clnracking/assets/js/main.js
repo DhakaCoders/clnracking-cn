7f885d8203fee3b7c885ab34a1d3ad042be64aee
Milon9787 <56488424+Milon9787@users.noreply.github.com>
2020-03-26 02:55:26 +0600

    footer responsive checking done

--- themes/clnracking/assets/js/main.js
+++ themes/clnracking/assets/js/main.js
@@ -276,6 +276,14 @@ if( $('.cr-tabs').length ){
   });
 }
 
+// footer slide menu
+$('.ftr-col h6').on('click', function(){
+  $(this).toggleClass('active');
+  $(this).parent().siblings().find('h6').removeClass('active');
+  $(this).parent().find('ul').slideToggle(300);
+  $(this).parent().siblings().find('ul').slideUp(300);
+});
+
 
 
 
